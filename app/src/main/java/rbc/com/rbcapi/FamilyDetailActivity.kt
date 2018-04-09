cbf2500130a6f2968cde3ea3016b50ce01d3cc4f
lukelin <dev.linxu@gmail.com>
2018-04-09 11:10:42 -0400

    step4: add FamilyDetailActivity.kt and ProductDetailActivity.kt

--- app/src/main/java/rbc/com/rbcapi/FamilyDetailActivity.kt
+++ app/src/main/java/rbc/com/rbcapi/FamilyDetailActivity.kt
@@ -5,33 +5,33 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import com.google.gson.GsonBuilder
-import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class FamilyDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        RestClient.restfulService.getProductFamilies().enqueue(object : Callback<JsonArray> {
-            override fun onFailure(call: Call<JsonArray>?, t: Throwable?) {
+        RestClient.restfulService.getProductFamily(intent.getStringExtra("data")).enqueue(object : Callback<JsonObject> {
+            override fun onFailure(call: Call<JsonObject>?, t: Throwable?) {
 
             }
 
-            override fun onResponse(call: Call<JsonArray>?, response: Response<JsonArray>?) {
+            override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>?) {
                 val body = response?.body()
                 if(body != null){
                     content.text = GsonBuilder().setPrettyPrinting().create().toJson(body)
-                    for(item in body){
-                        var btn = Button(this@MainActivity)
+                    for(item in body.get("products").asJsonArray){
+                        var btn = Button(this@FamilyDetailActivity)
                         var s = item.asJsonObject.get("href").asString
                         btn.text = s
                         btn.setOnClickListener {
-                            var intent = Intent(this@MainActivity, FamilyDetailActivity::class.java)
-                            intent.putExtra("data", s.replace("/families/", ""))
+                            var intent = Intent(this@FamilyDetailActivity, ProductDetailActivity::class.java)
+                            intent.putExtra("data", s.replace("/products/", ""))
                             startActivity(intent)
                         }
                         linear_layout.addView(btn)
---
+++ app/src/main/java/rbc/com/rbcapi/ProductDetailActivity.kt
@@ -0,0 +1,31 @@
+package rbc.com.rbcapi
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ProductDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        RestClient.restfulService.getProduct(intent.getStringExtra("data")).enqueue(object : Callback<JsonObject> {
+            override fun onFailure(call: Call<JsonObject>?, t: Throwable?) {
+
+            }
+
+            override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>?) {
+                val body = response?.body()
+                if(body != null){
+                    content.text = GsonBuilder().setPrettyPrinting().create().toJson(body)
+                }
+            }
+
+        })
+    }
+}
